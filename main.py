93873907c97c215bc00899123c5033700859f62e
NURSENA BİTİRGEN <94117345+senabibi@users.noreply.github.com>
2023-10-16 03:55:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("27aea069901ca81e93c552920d91bb95")
+account_sid = "YOUR ACCOUNT SID"
+auth_token = os.environ.get("AUTH_TOKEN_from_twilio")
+
+weather_params = {
+    "lat": "39.896519",
+    "lon": "32.861969",
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    proxy_client = TwilioHttpClient()
+    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+    client = Client(account_sid, auth_token, http_client=proxy_client)
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
+    )
+    print(message.status)
